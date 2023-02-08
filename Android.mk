3e05f92d4b28f7bfc192e30cfcae5db024d33feb
Chenyang Zhong <zhongcy95@gmail.com>
2023-02-08 23:51:06 +0800

    sdm660-common: move super_dummy build to Android.mk
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- Android.mk
+++ Android.mk
@@ -8,6 +8,20 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter tulip jasmine_sprout wayne clover lavender platina jason whyred,$(TARGET_DEVICE)),)
 
+LPFLASH := $(HOST_OUT_EXECUTABLES)/lpflash$(HOST_EXECUTABLE_SUFFIX)
+INSTALLED_SUPERIMAGE_DUMMY_TARGET := $(PRODUCT_OUT)/super_dummy.img
+
+$(INSTALLED_SUPERIMAGE_DUMMY_TARGET): $(PRODUCT_OUT)/super_empty.img $(LPFLASH)
+	$(call pretty,"Target dummy super image: $@")
+	$(hide) touch $@
+	$(hide) echo $(CURDIR)
+	$(hide) $(LPFLASH) $(CURDIR)/$@ $(CURDIR)/$(PRODUCT_OUT)/super_empty.img
+
+.PHONY: super_dummyimage
+super_dummyimage: $(INSTALLED_SUPERIMAGE_DUMMY_TARGET)
+
+INSTALLED_RADIOIMAGE_TARGET += $(INSTALLED_SUPERIMAGE_DUMMY_TARGET)
+
 include $(CLEAR_VARS)
 
 subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
