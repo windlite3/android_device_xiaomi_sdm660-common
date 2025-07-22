c5abd387aef2df4b38e752ca56987acce4e33e9a
pix106 <sbordenave@gmail.com>
2025-07-22 07:16:53 +0200

    sdm660-common: build libwfdservice_shim

--- sdm660.mk
+++ sdm660.mk
@@ -190,6 +190,7 @@ PRODUCT_PACKAGES += \
     libdisplayconfig \
     libdrmutils \
     libgralloc.qti \
+    libwfdservice_shim \
     vendor.display.config@1.0 \
     vendor.display.config@1.0.vendor \
     vendor.display.config@2.0 \
