c8a29de19fe9714a6429dc101ea50d87a4e97145
windlite <128964303+windlite3@users.noreply.github.com>
2025-08-14 09:11:45 +0800

    Update sdm660.mk

--- sdm660.mk
+++ sdm660.mk
@@ -642,8 +642,8 @@ PRODUCT_PACKAGES += \
     firmware_WCNSS_qcom_cfg.ini_symlink
 
 ifeq ($(WLAN_MAC_SYMLINK),true)
-PRODUCT_PACKAGES += \
-    firmware_wlan_mac.bin_symlink
+# PRODUCT_PACKAGES += \
+#    firmware_wlan_mac.bin_symlink
 else
 PRODUCT_PACKAGES += \
     firmware_wlan_mac.clover_symlink
