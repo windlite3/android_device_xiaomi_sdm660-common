257491a8445fbc92f36632a93dfef206cd53f12c
windlite <128964303+windlite3@users.noreply.github.com>
2025-08-15 17:37:40 +0800

    Update sdm660.mk

--- sdm660.mk
+++ sdm660.mk
@@ -314,8 +314,8 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(COMMON_PATH)/configs/keylayout/,$(TARGET_COPY_OUT_VENDOR)/usr/keylayout)
 
 # Keystore
-PRODUCT_PACKAGES += \
-    android.system.keystore2
+# PRODUCT_PACKAGES += \
+#     android.system.keystore2
 
 # Lights
 PRODUCT_PACKAGES += \
